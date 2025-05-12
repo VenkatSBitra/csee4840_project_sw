2d3ed3febd685ec553eb3a4d4bcbe3f36fae1a7f
doreen591 <ds4371@columbia.edu>
2025-05-12 04:13:05 -0400

    lr acc h

--- lr_acc.h
+++ lr_acc.h
@@ -18,7 +18,7 @@ typedef struct {
 #define LR_ACC_MAGIC 'q'
 
 /* ioctls and their arguments */
-#define LR_ACC_WRITE_DATA _IOW(LR_ACC_MAGIC, 1, lr_acc_arg_t)
-#define LR_ACC_READ_DATA  _IOR(LR_ACC_MAGIC, 2, lr_acc_arg_t)
+#define LR_ACC_WRITE_DATA _IOW(LR_ACC_MAGIC, 1, lr_acc_data_t)
+#define LR_ACC_READ_DATA  _IOR(LR_ACC_MAGIC, 2, lr_acc_data_t)
 
 #endif
