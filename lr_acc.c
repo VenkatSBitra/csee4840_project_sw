4f600ef0bc54eadd361e172ebdea80440f60b69f
doreen591 <ds4371@columbia.edu>
2025-05-12 17:30:30 -0400

    made change

--- lr_acc.c
+++ lr_acc.c
@@ -60,7 +60,7 @@ static void write_data(lr_acc_arg_t *data)
 	// short x = (data->data1 >> 4) & 0x0f | ((data->data2 >> 4) & 0x0f) << 4 | ((data->data3 >> 4) & 0x0f) << 8 | ((data->data4 >> 4) & 0x0f) << 12;
 	// short y = (data->data1 & 0x0f) | ((data->data2 & 0x0f) << 4) | ((data->data3 & 0x0f) << 8) | ((data->data4 & 0x0f) << 12);
 	if (data->go) {
-		iowrite32((u32)1, dev.virtbase + (1 << 9));
+		iowrite32((u32)1, dev.virtbase + (1 << 10));
 	} else {
 		iowrite32((u32)data->data.data, dev.virtbase + 4 * data->address);
 	}
